eb7fdbf3f37de01e1c2f6f2c256c565b6ccb5904
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-30 18:28:45 -0400

    update tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} font-sans antialiased bg-black text-white`}>
         {children}
       </body>
     </html>
